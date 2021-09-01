c744c63ebdb5ae8d4f60ccce835c9ea7bcdb0ac7
han <myinttun.han@germs.sg>
2021-09-01 23:26:48 +0800

    init

---
+++ ReminderApp/ContentView.swift
@@ -0,0 +1,80 @@
+//
+//  ContentView.swift
+//  ReminderApp
+//
+//  Created by Han on 1/9/21.
+//
+
+import SwiftUI
+import UserNotifications
+import CoreLocation
+class NotificationManager{
+    static let instance = NotificationManager()
+    
+    func reqAuthorization(){
+        let options: UNAuthorizationOptions = [.alert,.sound,.badge]
+        UNUserNotificationCenter.current().requestAuthorization(options: options) {(success, error) in
+            if let error = error{
+                print("Error: \(error)")
+            }else {
+                print("Success")
+            }
+        }
+    }
+    
+    func scheduleNotification(){
+        let content = UNMutableNotificationContent()
+        content.title = "This is first Noti"
+        content.subtitle = "This is cool"
+        content.sound = .default
+        content.badge = 1
+        
+        //by time interval
+        let triggerTime = UNTimeIntervalNotificationTrigger(timeInterval: 5.0, repeats: false)
+        
+        //by calendar
+        var dateComponents = DateComponents()
+        dateComponents.hour = 10
+        dateComponents.minute = 0
+        dateComponents.weekday = 5
+        let triggerDate = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        
+        //by location
+        
+        
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: triggerDate)
+        UNUserNotificationCenter.current().add(request)
+    }
+    
+}
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack(spacing:40) {
+            
+            Button("Request Permission") {
+                // first request noti permission
+                NotificationManager.instance.reqAuthorization()
+            }
+
+            Button("Schedule Notification") {
+                // second schedule noti
+                NotificationManager.instance.scheduleNotification()
+                
+            }
+        }
+        .onAppear{
+            
+            //clear badge number
+            UIApplication.shared.applicationIconBadgeNumber = 0
+        }
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ReminderApp/ReminderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderAppApp.swift
+//  ReminderApp
+//
+//  Created by Han on 1/9/21.
+//
+
+import SwiftUI
+
+@main
+struct ReminderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
